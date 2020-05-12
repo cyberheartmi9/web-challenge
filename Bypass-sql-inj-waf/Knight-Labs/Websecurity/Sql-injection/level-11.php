b5a5294101d69d46911f2247e16d641220a24fb9
PikaChu <abdelazimmohmmed@gmail.com>
2020-05-13 02:27:59 +0300

    Create level-11.php

---
+++ Bypass-sql-inj-waf/Knight-Labs/Websecurity/Sql-injection/level-11.php
@@ -0,0 +1,51 @@
+<?php
+
+
+require "../../Core/WAF/Sql-inj/Connection-inc.php";
+require "../../Core/WAF/Sql-inj/Crypto.php";
+
+
+
+
+$method="aes-256-cbc";
+$password="secret";
+
+
+
+
+$data=$_GET['id'];
+
+
+$dec=new Crypto($data,$password,$method);
+$id=$dec->Decrypt();
+
+
+if($id==false){
+
+
+    echo "<h1> cant Decrypt it !!!!!!</h1>";
+
+}
+
+
+
+else {
+
+    $sql = "SELECT * FROM `users` WHERE id=$id";
+    $re = mysql_query($sql);
+
+    if ($row = mysql_fetch_array($re)) {
+
+        echo "Name : " . $row['username'] . "<br> Password :" . $row['password'] . "<br> Email :" . $row['email'];
+
+
+    } else {
+        echo '<font color= "#0000FF">';
+        print_r(mysql_error());
+        echo "</font>";
+    }
+
+}
+
+
+?>
